71f22dfa3a8548aba2d4c5eae505030b16ea01ec
chxiaoyu <961041421@qq.com>
2020-04-09 20:50:35 +0800

    提交所有删除

--- day01/js/itany.js
+++
@@ -1,7 +0,0 @@
-console.log("welcome to wbs16073!");
-
-/*
-	关于路径问题：
-	1.js是根据引入它的html文件所在位置
-	2.css是根据当前css文件所在位置
-*/
--- day05/js/itany.js
+++
@@ -1,7 +0,0 @@
-function calcSum(){
- 	var sum=0;
-	 for(var i=1;i<=100;i++){
-	 	sum+=i;
-	 }
-	 console.log(sum);
- }
